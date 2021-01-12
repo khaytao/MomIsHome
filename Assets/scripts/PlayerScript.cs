24b0683a0b920628ca33ada87e25daa504e155c1
kfir <kfir.hayman@gmail.com>
2021-01-12 15:49:56 +0200

    changed movement to arrows

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -121,6 +121,29 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 direction = _goal - _rb.position;
+        
+        // keyboard input
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+        
         _rb.velocity = speed * direction.normalized;
 
         if (!isFixing && direction.magnitude < goal_threshold)
