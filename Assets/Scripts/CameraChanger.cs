55e71f1937dbad40d82d04bb2a3746b3e528d233
kfir <kfir.hayman@gmail.com>
2021-01-12 16:21:10 +0200

    pushing

---
+++ Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public Camera camOver;
+    public Camera camZoomed;
+
+    private void Start()
+    {
+        camOver.enabled = true;
+        camZoomed.enabled = false;
+        StartCoroutine(ExecuteAfterTime(3));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+ 
+        camOver.enabled = false;
+        camZoomed.enabled = true;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C)) 
+        {
+            camOver.enabled = !camOver.enabled;
+            camZoomed.enabled = !camZoomed.enabled;
+        }
+    }
+}
