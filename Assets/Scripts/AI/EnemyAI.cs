f516b68720d70001649dd84647bff0f753f9a259
kfir <kfir.hayman@gmail.com>
2021-01-19 14:37:18 +0200

    ai enmeny done minus animations

---
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public List<Transform> pointsList;
+    public float thresh = 0.1f;
+    private AIPath path;
+    private AIDestinationSetter ds;
+
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        ds = GetComponent<AIDestinationSetter>();
+        path = GetComponent<AIPath>();
+        ds.target = pointsList[i];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, pointsList[i].position) < thresh)
+        {
+            Debug.Log("here");
+            i = (i + 1) % pointsList.Count;
+            ds.target = pointsList[i];
+            
+        }
+    }
+}
