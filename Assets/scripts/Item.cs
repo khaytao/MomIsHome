c3738538305b40906e14b3aff4c136c5bc1c208d
fouadhal <fouad.hallomi@mail.huji.ac.il>
2021-01-19 03:08:01 +0200

    added Interactable in animation

--- Assets/scripts/Item.cs
+++ Assets/scripts/Item.cs
@@ -6,10 +6,36 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public TaskType forTaskType;
+    private Animator animator;
+    private bool isInteractable;
     [HideInInspector] public Collider2D itemCollider;
     void Start()
     {
+        isInteractable = false;
         itemCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
         GameManager.Instance.addItem(this);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            animator.SetBool("Interactable", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            animator.SetBool("Interactable", false);
+        }
+    }
+    
+    public void triggerInteractable()
+    {
+        isInteractable = !isInteractable;
+        animator.SetBool("Interactable", isInteractable);
     }
 }
--- Assets/scripts/Task.cs
+++ Assets/scripts/Task.cs
@@ -8,7 +8,7 @@ public enum TaskType
     Fire,
     Trash,
     Repair,
-    Sweep
+    Sweep,
 }
 
 public class Task : MonoBehaviour
@@ -16,13 +16,17 @@ public class Task : MonoBehaviour
     public float duration;
     public TaskType type;
     private SpriteRenderer childWaterRenderer;
+    private bool isInteractable;
+    private Animator animator;
     [HideInInspector] public Collider2D taskCollider;
     [HideInInspector] public Collider2D circleCollider;
     [HideInInspector] public SpriteRenderer taskRenderer;
     void Start()
     {
+        isInteractable = false;
         taskCollider = GetComponent<Collider2D>();
         taskRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         GameManager.Instance.addTask(this);
         if (type == TaskType.Sweep)
         {
@@ -38,5 +42,27 @@ public class Task : MonoBehaviour
             childWaterRenderer.transform.localScale += Time.deltaTime * 0.1f * (Vector3)Vector2.one;
             duration += Time.deltaTime * 0.1f;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            animator.SetBool("Interactable", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            animator.SetBool("Interactable", false);
+        }
+    }
+
+    public void triggerInteractable()
+    {
+        isInteractable = !isInteractable;
+        animator.SetBool("Interactable", isInteractable);
     }
 }
