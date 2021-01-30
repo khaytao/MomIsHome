924561e18da17fb1c8ffe422e7b7ece28d4ee8c8
fouadhal <fouad.hallomi@mail.huji.ac.il>
2021-01-30 02:23:55 +0200

    added shaking time frames and sounds

--- Assets/scripts/TimeScript.cs
+++ Assets/scripts/TimeScript.cs
@@ -21,6 +21,12 @@ public class TimeScript : MonoBehaviour
     // 360 / 60 / 12
     private float hourAnglePerMinute = 0.5f;
     private Vector3 goalHourAngle;
+
+    private float shakeStarted;
+
+    private float shakeForce;
+
+    public float shakeLength;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +41,7 @@ public class TimeScript : MonoBehaviour
         minuteHand.transform.eulerAngles = minuteAngle;
         
         A0 = transform.position;
+        shakeStarted = -1;
     }
 
     // Update is called once per frame
@@ -43,22 +50,51 @@ public class TimeScript : MonoBehaviour
 
         Vector3 hourAngle = Time.deltaTime * (gameTimeFactor / 60) * hourAnglePerMinute * Vector3.forward;
         hourHand.transform.eulerAngles -= hourAngle;
-
-        if (GetSecondsLeft() < 30)
-        {
-            //speed = 700;
-            //amount = 1.1f;
-            float shake_x = Mathf.Sin(speed * Time.time) * amount;
-            
-            transform.position = A0 + new Vector3(shake_x,  0, 0);
-        }
+        
+        shakeClock();
+        
         if(GetSecondsLeft() <= 0)
-        {
-            GameManager.Instance.timeOver();
-        }
+            GameManager.Instance.timeOver(); 
 
         Vector3 minuteAngle = Time.deltaTime * (gameTimeFactor / 60) * anglePerMinute * Vector3.forward;
         minuteHand.transform.eulerAngles -= minuteAngle;
+    }
+
+    private void shakeClock()
+    {
+        float elapsedTime = GameManager.Instance.elapsedTime;
+        if (shakeStarted >= 0 && elapsedTime - shakeStarted < shakeLength)
+        {
+            float shake_x = Mathf.Sin(speed * Time.time) * shakeForce;
+            transform.position = A0 + new Vector3(shake_x,  0, 0);
+            return;
+        }
+
+        float timeLeft = GetSecondsLeft();
+        transform.position = A0;
+        // 90 seconds left
+        if (timeLeft <= 90 && timeLeft >= 90 - shakeLength)
+            startShake(1, AudioFileGetter.i.timeLeft90);
+
+        // 60 seconds
+        if (timeLeft <= 60 && timeLeft >= 60 - shakeLength)
+            startShake(3, AudioFileGetter.i.timeLeft60);
+
+        // 30 seconds
+        if (timeLeft <= 30 && timeLeft >= 30 - shakeLength)
+            startShake(5, AudioFileGetter.i.timeLeft30);
+
+        // 15 seconds
+        if (timeLeft <= 15 && timeLeft >= 15 - shakeLength)
+            startShake(7, AudioFileGetter.i.timeLeft15);
+    }
+
+    private void startShake(float shakeForce, AudioClip clip)
+    {
+        shakeStarted = GameManager.Instance.elapsedTime;
+        this.shakeForce = shakeForce;
+        SoundManager.PlaySound(clip);
+        // todo: should shakeLength change per frame?
     }
 
     private float GetSecondsLeft()
